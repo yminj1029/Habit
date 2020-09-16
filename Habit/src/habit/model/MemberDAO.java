ad622ce9c8319b119fcc44f20f8b5556b1348a4e
SMT050 <SMT050@112.187.117.251>
2020-09-16 15:51:48 +0900

    박한성 : memberDAO 만듬

---
+++ Habit/src/habit/model/MemberDAO.java
@@ -0,0 +1,32 @@
+package habit.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class MemberDAO {
+	private Connection conn;
+	private PreparedStatement ps;
+	private ResultSet rs;
+	// 초기화 블럭
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Connection getConnect() {
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
+		String user = "hr";
+		String password = "hr";
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+}
