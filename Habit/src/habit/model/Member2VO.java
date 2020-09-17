42cf40b7af018ee572d6c2f5d5270de0ec5d3f9f
SMT079 <SMT079@183.105.60.142>
2020-09-17 12:12:06 +0900

    yun minji : vo member2, review, newhabit

---
+++ Habit/src/habit/model/Member2VO.java
@@ -0,0 +1,112 @@
+package habit.model;
+
+public class Member2VO {
+	//속성을 만들자!!!!
+	
+	private String m_id; 
+	private String pw; 
+	private String name; 
+	private String nickname; 
+	private String tel; 
+	private String gender;
+	private String job; 
+	private String email;
+	private int point;
+	private String category;
+	
+	public Member2VO(String m_id, String pw, String name, String nickname, String tel, String gender, String job,
+			String email, int point, String category) {
+		super();
+		this.m_id = m_id;
+		this.pw = pw;
+		this.name = name;
+		this.nickname = nickname;
+		this.tel = tel;
+		this.gender = gender;
+		this.job = job;
+		this.email = email;
+		this.point = point;
+		this.category = category;
+	}
+
+	public String getM_id() {
+		return m_id;
+	}
+
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTell(String tel) {
+		this.tel = tel;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+}
