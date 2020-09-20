5b4bfeea2623cc4f77aa65273dde63dd8fd4fb5e
USER <USER@DESKTOP-PGRNQFQ>
2020-09-21 01:55:43 +0900

    박한성 : challengeResultDAO 이름수정 첼린지 결과 생성 기능 추가 그에 따른 VO 수정

---
+++ 2Habit/src/habit/model/ChallengeResultDAO.java
@@ -0,0 +1,71 @@
+package habit.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ChallengeResultDAO {
+	public class Member2DAO {
+		private Connection conn;
+		private PreparedStatement ps;
+		private ResultSet rs;
+
+		// 초기화 블럭
+		public Connection getConnect() {
+			String url = "jdbc:oracle:thin:@112.187.117.251:1521:XE";
+			String user = "hv";
+			String password = "hv";
+			try {
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				conn = DriverManager.getConnection(url, user, password);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return conn;
+		}
+
+		public void dbClose() {
+			try {
+				if (rs != null)
+					rs.close();
+				if (ps != null)
+					ps.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		public ArrayList<ChallengeResultVO> challengeResult(Member2VO vocm) {
+			vocm = new Member2VO();
+			ArrayList<ChallengeResultVO> list = new ArrayList<ChallengeResultVO>();
+			String sql = "select c.ch_name, c.startdate, c.enddate, cr.chr_result"
+					+ "from challenge c, challenge_result cr where c.m_id=?";
+			try {
+				getConnect();
+				ps = conn.prepareStatement(sql);
+				ps.setString(1, vocm.getM_id());
+				rs = ps.executeQuery();
+				while (rs.next()) {
+					String ch_name = rs.getString(1);
+					String startdate = rs.getString(2);
+					String enddate = rs.getString(3);
+					String chr_result = rs.getString(4);
+
+					ChallengeResultVO Vot = new ChallengeResultVO(ch_name, startdate, enddate, chr_result);
+					list.add(Vot);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return list;
+
+		}
+
+	}
+}
--- 2Habit/src/habit/model/ChallengeResultVO.java
+++ 2Habit/src/habit/model/ChallengeResultVO.java
@@ -3,75 +3,46 @@ package habit.model;
 public class ChallengeResultVO {
 	//속성을 만들자!!!!
 	
-	private int ch_id; 
-	private int chm_id; 
-	private int prf_id;
-	private String chr_date;
-	private int chr_point;
+	//private int ch_id; 
+	//private int chm_id; 
+	//private int prf_id;
+	private String ch_name;
+	private String startdate;
+	private String enddate;
 	private String chr_result;
-	
-	public ChallengeResultVO() {
+	public ChallengeResultVO(String ch_name, String startdate, String enddate, String chr_result) {
 		super();
-	}
-
-	public ChallengeResultVO(int ch_id, int chm_id, int prf_id, String chr_date, int chr_point, String chr_result) {
-		super();
-		this.ch_id = ch_id;
-		this.chm_id = chm_id;
-		this.prf_id = prf_id;
-		this.chr_date = chr_date;
-		this.chr_point = chr_point;
+		this.ch_name = ch_name;
+		this.startdate = startdate;
+		this.enddate = enddate;
 		this.chr_result = chr_result;
 	}
-
-	public int getCh_id() {
-		return ch_id;
+	public String getCh_name() {
+		return ch_name;
 	}
-
-	public void setCh_id(int ch_id) {
-		this.ch_id = ch_id;
+	public void setCh_name(String ch_name) {
+		this.ch_name = ch_name;
 	}
-
-	public int getChm_id() {
-		return chm_id;
+	public String getStartdate() {
+		return startdate;
 	}
-
-	public void setChm_id(int chm_id) {
-		this.chm_id = chm_id;
+	public void setStartdate(String startdate) {
+		this.startdate = startdate;
 	}
-
-	public int getPrf_id() {
-		return prf_id;
+	public String getEnddate() {
+		return enddate;
 	}
-
-	public void setPrf_id(int prf_id) {
-		this.prf_id = prf_id;
+	public void setEnddate(String enddate) {
+		this.enddate = enddate;
 	}
-
-	public String getChr_date() {
-		return chr_date;
-	}
-
-	public void setChr_date(String chr_date) {
-		this.chr_date = chr_date;
-	}
-
-	public int getChr_point() {
-		return chr_point;
-	}
-
-	public void setChr_point(int chr_point) {
-		this.chr_point = chr_point;
-	}
-
 	public String getChr_result() {
 		return chr_result;
 	}
-
 	public void setChr_result(String chr_result) {
 		this.chr_result = chr_result;
 	}
-		
+	
+	
 	
 	
 }
